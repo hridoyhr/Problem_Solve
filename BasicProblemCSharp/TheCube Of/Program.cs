6ce8ef74391daa8cf51f73c17cf2526ff831d650
hridoyhr <hridoyswe205@gmail.com>
2021-02-25 01:33:01 +0600

    Basic Problem in C#

---
+++ BasicProblemCSharp/TheCube Of/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TheCube_Of
+{
+    class Program
+    {
+        public static double Cube(double num)
+        {
+            return num * num * num;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cube(2));
+            Console.WriteLine(Cube(-5.5));
+            Console.WriteLine(Cube(5));
+        }
+    }
+}
